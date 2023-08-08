0ede8d8d6c463184186d5c99f520dbcd27817518
Brahim-BT <baibbat.brahim@gmail.com>
2023-08-08 23:47:16 +0100

    Resolving

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * argstostr - This function will concatenates all the arguments of my program
+ * @ac: honestly I don't know yet what it is
+ * @av: honestly I don't know yet what it is
+ * Return: nothing
+ */
+
+char *argstostr(int ac, char **av)
+{
+    char *p;
+
+    p = 'R';
+    return (p);
+}
