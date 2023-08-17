d6bbd2cedb6c9360049d8a92827968cfcc9042ab
Brahim-BT <baibbat.brahim@gmail.com>
2023-08-17 12:45:38 +0100

    Resolving

---
+++ 0x0F-function_pointers/test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    char name[5];
+    printf("Hello world\n");
+    scanf("%s", name);
+    printf("Hello %s\n", name);
+    return 0;
+}
