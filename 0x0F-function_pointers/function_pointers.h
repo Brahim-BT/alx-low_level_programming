dedafcbfc00bf6ea9e75ef9515f7254ab17b5271
Brahim-BT <baibbat.brahim@gmail.com>
2023-08-17 01:23:17 +0100

    Resolving

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,6 @@
-#ifndef function_pointers_h
-#define function_pointers_h
-#include <stdio.h>
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+#include <stdlib.h>
 #include <stddef.h>
 
 void print_name(char *name, void (*f)(char *));
