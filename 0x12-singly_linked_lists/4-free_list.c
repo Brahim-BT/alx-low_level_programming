5213f211c9c3d9259f52ca3f66aa622cba397635
Brahim-BT <baibbat.brahim@gmail.com>
2023-08-25 00:52:52 +0100

    Resolving

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * add_node_end - this function will free a list_t list.
+ * @head: the head of list_t
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+    if (head == NULL)
+        return;
+    list_t *newhead;
+
+    newhead = head;
+
+    while (newhead != NULL)
+    {
+        free(newhead->str);
+        newhead = newhead->next;
+    }
+    free(head);
+}
