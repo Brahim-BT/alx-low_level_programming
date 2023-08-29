326f5ab2055d10986d84153d18dd62cd4a731d8f
Brahim-BT <baibbat.brahim@gmail.com>
2023-08-30 00:27:48 +0100

    Resolving

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,10 +13,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
     listint_t *node;
 
-    node = head;
-    i = 0;
     if (head == NULL)
         return (NULL);
+    node = head;
+    i = 0;
     while (node != NULL)
     {
         if (i == index)
