7a81dca67d4f583d13b01da2342a658c69e6b814
Brahim-BT <baibbat.brahim@gmail.com>
2023-08-31 22:47:32 +0100

    Resolving

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -20,7 +20,25 @@ unsigned int binary_to_uint(const char *b)
         if (b[i] != '1' && b[i] != '0')
             return (0);
         if (b[i] == '1')
-            decnum += pow(2, k);
+            decnum += power(2, k);
     }
     return (decnum);
+}
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int.
+ * @b: binary number
+ * Return: int or 0.
+ */
+
+int power(int b, int e)
+{
+    int result = 1;
+
+    for (int i = 0; i < e; i++)
+    {
+        result *= b;
+    }
+
+    return result;
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,7 +4,6 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <math.h>
 
 unsigned int binary_to_uint(const char *b);
 
