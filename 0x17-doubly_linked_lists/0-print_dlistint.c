ff00942b1592fee4e9c2670979b13b1ef84a1ba0
Brahim-BT <baibbat.brahim@gmail.com>
2023-10-12 19:09:55 +0100

    Resolving

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - print the elements of a dlinkedlist
+ * @h: pointer to the double linked list
+ * Return: integer
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+    int n;
+
+    n = 0;
+
+    while (h != NULL)
+    {
+        printf("%d", (*h).n);
+        n++;
+        h = (*h).next;
+    }
+    return n;
+}
---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This the directory of 0x17-doubly_linked_lists
