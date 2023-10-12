ed3cb80287005779bdb7e123ca7a6a1afb647431
Brahim-BT <baibbat.brahim@gmail.com>
2023-10-12 23:19:30 +0100

    Resolving

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -16,15 +16,18 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
         return NULL;
     new_node->n = n;
     new_node->next = NULL;
-    if ((*head) == NULL)
+    if ((*head) != NULL)
     {
-        new_node->prev = NULL;
-        (*head) == new_node;
-        return (new_node);
+        while ((*head)->next != NULL)
+            (*head) = (*head)->next;
+        (*head)->next = new_node;
+        new_node->prev = (*head);
+        (*head) = new_node;
     }
-    while ((*head)->next != NULL)
-        (*head) = (*head)->next;
-    (*head)->next = new_node;
-    new_node->prev = (*head);
+    else
+    {
+        (*head)->next = new_node;
+        new_node->prev = (*head);
+    }
     return (new_node);
 }
